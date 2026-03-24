56b0fc331ecdeec51277d781549988ba7bde4e7f
JunzheShi0702 <jshi70@jh.edu>
2026-03-23 21:22:52 -0400

    Fix ESLint no-namespace error for Express module augmentation (#131)

--- backend/src/types/database.ts
+++ backend/src/types/database.ts
@@ -82,6 +82,7 @@ export type AuthUser = z.infer<typeof authUserSchema>;
 
 // Extended request types for Express
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace Express {
     interface Request {
       user?: AuthUser;
